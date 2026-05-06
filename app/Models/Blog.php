cd114a8dd18bb2838de44c6d566a636553d86920
abukbt13 <abukbt13@gmail.com>
2026-05-06 14:37:18 +0300

    remove hasId

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,16 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Vinkla\Hashids\Facades\Hashids;
 
 class Blog extends Model
 {
     use HasFactory;
-    public function getHashIdAttribute()
-    {
-        return Hashids::encode($this->id);
-    }
-    protected $appends = ['hash_id'];
+
 
     protected $fillable = ['user_id', 'title', 'summary', 'content', 'image'];
 
